f48b856359742d62b417db3f2bd7d64810300fb5
abdullahabdelshefi-arch <abdullah.abdelshefi@gmail.com>
2026-02-27 11:02:46 -0600

    chnages done just bounce left

--- interactive-sence/sketch.js
+++ interactive-sence/sketch.js
@@ -1,13 +1,13 @@
+
 // Interactive sence
 // Abdulah Abdelshafi
 // Febuary 24 2026
 //
 // Extra for Experts:
 // I made it so that when you scrool the mouse wheel up it makes it harder and the speed faster and if you scrool down it makes it slower so it is easier. 
-// I also inculed an image background that chnages from the loading screen to the start screen 
 
 
-// defining variables
+// Defining variables
 let rx;
 let ry;
 let x;
@@ -21,7 +21,7 @@ let h = 50;
 let startX;
 let StartY;
 
-// stetting up the display
+// Stetting up the display
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
@@ -30,15 +30,15 @@ function setup() {
   x = width / 2;
   y = height / 2;
   w = 100;
-  startX = x - h;
-  startY = y + h;
+  startX = x - 50;
+  startY = y + 50;
   xSpeed = random(3, 4);
   ySpeed = random(3, 4);
 }
 
-// drawing the whole thing
+// Drawing the whole thing
 function draw() {
-  background("red");
+  background("black");
   if (state === "loading") {
     rect(startX, startY, w, h);
     textSize(70);
@@ -46,8 +46,9 @@ function draw() {
     textAlign(CENTER);
     text("Press Rectangle To Start", width / 2, height / 2);
   }
-  // if the state is play the game works
+  // If the state is play the game works
   if (state === "play") {
+    background("red");
     displayRect();
     displayCircle();
     moveRect();
@@ -57,7 +58,7 @@ function draw() {
     gameOver();
   }
 
-  // if the ball passes write game over
+  // If the ball passes write game over
   if (state === "gameOver") {
     textSize(70);
     fill("white");
@@ -65,18 +66,19 @@ function draw() {
     text("Game Over", width / 2, height / 2);
   }
 }
+  
 
-// if the mouse is pressed on the rectangle under the start button
+// If the mouse is pressed on the rectangle under the start button
 function mousePressed() {
   if (mouseX > startX && mouseX < startX + w && mouseY > startY && mouseY < startY + h) {
     state = "play";
   }
 }
 
-//  extra for experts when the mouse is scrolled up it increases the speed and down decreases (recourses and google)
+// Extra for experts when the mouse is scrolled up it increases the speed and down decreases(reasources and google)
 function mouseWheel(event) {
   // Down
-  if (event.delta > 0) {
+  if (event.delta > 0 ) {
     xSpeed *= 0.9;
     ySpeed *= 0.9;
   } 
@@ -87,7 +89,7 @@ function mouseWheel(event) {
   }
 }
 
-// display the rectangle and circle
+// Display the rectangle and ball
 function displayRect() {
   fill("black");
   rect(rx, ry, w, h, 20);
@@ -97,7 +99,7 @@ function displayCircle() {
   circle(x, y, radius);
 }
 
-// move rect and circle
+// Move rectangle and ball
 function moveRect() {
   if (keyIsDown(68) === true) {
     rx += 5;
@@ -106,35 +108,38 @@ function moveRect() {
     rx -= 5;
   }
 }
+ 
 function moveBall() {
   x += xSpeed;
   y += ySpeed;
 }
 
-// bouncing off the wall
+// Bouncing off the wall
 function bounceOfWall() {
+  // side wall
   if (x + radius / 2 > width || x - radius / 2 < 0) {
     xSpeed *= -1;
   }
+  // top wall
   if (y - radius / 2 < 0) {
     ySpeed *= -1;
   }
 }
 
-// boucing ball of the rectangle
+// Boucing ball of the rectangle
 function rectBounce() {
-  if (y + radius / 2 >= ry / 2 && x > rx && x < rx + w) {
+  if (y + radius / 2 >= ry && x > rx && x < rx + w) {
     ySpeed *= -1;
   }
 }
 
-// checking if the game is over
+// Checking if the game is over
 function gameOver() {
   if (y - radius / 2 > height) {
     state = "gameOver";
   }
 }
 
-
-// is the form correct 
-// how do I fix if it touches the end of rectangle 
+// while loop if it is still contacting the rect from the top kepp going up till it isnt touching down keep moving down till it isnt touching
+// while y + radius/2 <= top: ySpeed * -1
+// while y + raduis/2 >= bottom: ySpeed * 1
