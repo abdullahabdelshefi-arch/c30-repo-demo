79ef38bfc736ce23172d80c3a3497705dc5cabcb
abdullahabdelshefi-arch <abdullah.abdelshefi@gmail.com>
2026-03-04 11:00:52 -0600

    ball array

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -1,10 +1,6 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Ball Array Demo
 
+let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +8,37 @@ function setup() {
 
 function draw() {
   background(220);
+
+  for (let ball of ballArray) {
+    //move
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    //  telaport
+    if(ball.x - ball.radius > width){
+      ball.x -= -ball.raduis;
+    }
+      
+    //display
+    fill(ball.r,ball.g,ball.b);
+    circle(ball.x, ball.y, ball.radius*2);
+  
+}
+}
+function mousePressed() {
+  spawnBall(mouseX,mouseY);
+}
+
+function spawnBall(_x,_y) {
+  let someBall = {
+    x: _x,
+    y: _y,
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+    radius: random(10, 30),
+    r: random(0,255),
+    g: random(0,255),
+    b: random(0,255),
+  };
+  ballArray.push(someBall);
 }
