d6a5152f05d3704426893bb5c7a742874a7153c8
abdullahabdelshefi-arch <abdullah.abdelshefi@gmail.com>
2026-03-05 10:05:49 -0600

    Update sketch.js

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -1,44 +1,13 @@
-// Ball Array Demo
-
-let ballArray = [];
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
+  background("black");
+  stroke("white");
+  let circleX = 200;
+  let circleY = 200;
+  let circleDiameter = 100;
 }
 
 function draw() {
-  background(220);
-
-  for (let ball of ballArray) {
-    //move
-    ball.x += ball.dx;
-    ball.y += ball.dy;
-
-    //  telaport
-    if(ball.x - ball.radius > width){
-      ball.x -= -ball.raduis;
-    }
-      
-    //display
-    fill(ball.r,ball.g,ball.b);
-    circle(ball.x, ball.y, ball.radius*2);
-  
-}
-}
-function mousePressed() {
-  spawnBall(mouseX,mouseY);
-}
-
-function spawnBall(_x,_y) {
-  let someBall = {
-    x: _x,
-    y: _y,
-    dx: random(-5, 5),
-    dy: random(-5, 5),
-    radius: random(10, 30),
-    r: random(0,255),
-    g: random(0,255),
-    b: random(0,255),
-  };
-  ballArray.push(someBall);
+  circleDiameter = circleDiameter + 50;
+  ellipse(circleX, circleY, circleDiameter, circleDiameter);
 }
