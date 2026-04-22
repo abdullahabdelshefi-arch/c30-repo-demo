f3dd44a8f8bc2d5622f6be6375cdd592fea3065f
abdullahabdelshefi-arch <abdullah.abdelshefi@gmail.com>
2026-04-22 11:03:04 -0600

    Set up the classes and varibles understood null

--- 15-nodes/sketch.js
+++ 15-nodes/sketch.js
@@ -1,76 +1,6 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Connected Nodes OOP Demo
 
 let nodes = [];
-
-class MovingPoint{
-  constructor(x,y){
-    this.x = x;
-    this.y = y; 
-    this.xTime = random(1000);
-    this.yTime = random(1000);
-    this.deltaTime = 0.05;
-    this.radius = 15;
-    this.speed = 5;
-    this.color = color(random(255), random(255), random(255));
-    this.reach = 100;
-  }
-  display(){
-    noStroke;
-    fill(this.color);
-    circle(this.x, this.y, this.radius*2);
-  }
-  update(){
-    this.move();
-    this.wrap();
-  }
-
-  connectTo(nodesArray){
-    for (let otherNode of nodesArray){
-      if (this!== otherNode){
-        let distanceAway = dist(this.x, this.y, otherNode.x, otherNode.y);
-        if (distanceAway < this.reach){
-          stroke(this.color);
-          line (this.x,this.y,otherNode.x, otherNode.y);
-        }
-      }
-    }
-  }
-
-  wrap(){
-    if (this.x < 0){
-      this.x -= width;
-    }
-    if (this.y < 0){
-      this.y -= height;
-    }
-    if (this.x > 0){
-      this.x += width;
-    }
-    if (this.y > 0){
-      this.y += height;
-    }
-  }
-  
-  move(){
-    let dx = noise(this.xTime);
-    let dy = noise(this.yTime);
-
-    // scale 0 to 1 
-    dx = map(dx, 0, 1, -this.speed, this.speed);
-    dy = map(dy, 0 , 1,-this.speed, this.speed);
-
-    this.x += dx;
-    this.y += dy;
-
-    this.xTime += this.deltaTime;
-    this.yTime += this.deltaTime;
-  }
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -78,14 +8,106 @@ function setup() {
 
 function draw() {
   background(220);
-  for (let node of nodes){
-    node.display();
+
+  //draw lines first
+  for (let node of nodes) {
     node.update();
     node.connectTo(nodes);
   }
+
+  //draw the circles after
+  for (let node of nodes) {
+    node.display();
+  }
 }
 
-function mousePressed(){
+function mousePressed() {
   let somePoint = new MovingPoint(mouseX, mouseY);
   nodes.push(somePoint);
+}
+
+
+class MovingPoint {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.xTime = random(1000);
+    this.yTime = random(1000);
+    this.deltaTime = 0.05;
+    this.radius = 15;
+    this.speed = 5;
+    this.color = color(random(255), random(255), random(255));
+    this.reach = 200;
+    this.minRadius = 15;
+    this.maxRadius = 50;
+  }
+
+  display() {
+    noStroke();
+    fill(this.color);
+    circle(this.x, this.y, this.radius*2);
+  }
+
+  update() {
+    this.move();
+    this.wrapAroundScreen();
+    this.adjustSizeByMouse();
+  }
+
+  adjustSizeByMouse() {
+    let mouseDistance = dist(this.x, this.y, mouseX, mouseY);
+    if (mouseDistance < this.reach) {
+      let theSize = map(mouseDistance, 0, this.reach, this.maxRadius, this.minRadius);
+      this.radius = theSize;
+    }
+    else {
+      this.radius = this.minRadius;
+    }
+  }
+
+  connectTo(nodesArray) {
+    for (let otherNode of nodesArray) {
+      if (this !== otherNode) {
+        let distanceAway = dist(this.x, this.y, otherNode.x, otherNode.y);
+        if (distanceAway < this.reach) {
+          stroke(this.color);
+          line(this.x, this.y, otherNode.x, otherNode.y);
+        }
+      }
+    }
+  }
+
+  wrapAroundScreen() {
+    if (this.x + this.radius < 0) {
+      this.x += width;
+    }
+    if (this.x - this.radius > width) {
+      this.x -= width;
+    }
+    if (this.y + this.radius < 0) {
+      this.y += height;
+    }
+    if (this.y - this.radius > height) {
+      this.y -= height;
+    }
+  }
+
+
+  move() {
+    let dx = noise(this.xTime);
+    let dy = noise(this.yTime);
+
+    // console.log(dx, dy);
+    //scale from 0-1 to movement speed
+    dx = map(dx, 0, 1, -this.speed, this.speed);
+    dy = map(dy, 0, 1, -this.speed, this.speed);
+
+    //move point
+    this.x += dx;
+    this.y += dy;
+
+    //move on the time axis
+    this.xTime += this.deltaTime;
+    this.yTime += this.deltaTime;
+  }
 }
